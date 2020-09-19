a3281b4dbd609c612e7e3271f6ee2b235b629e7f
Anders Johansson <andersjo@gmail.com>
2020-09-19 15:45:43 +0200

    Update Adroid build and target versoins

--- src/com/turfgame/json/ParseJSON.java
+++ src/com/turfgame/json/ParseJSON.java
@@ -1,29 +1,26 @@
 package com.turfgame.json;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.StatusLine;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import android.util.Log;
 
 import com.turfgame.widget.CharStats;
 import com.turfgame.widget.TurfWidget;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class ParseJSON {
-	private static final String URL_ADDRESS = "http://api.turfgame.com/v4/users";
+	private static final String URL_ADDRESS = "https://api.turfgame.com/v4/users";
 	private static final String DEBUG_TAG = "ParseJSON";
 
 	public static CharStats parseJSON(String email) {
@@ -44,51 +41,52 @@ public class ParseJSON {
 			}
 		}
 
-		StringBuilder stringBuilder = new StringBuilder();
-		HttpClient httpClient = new DefaultHttpClient();
-		HttpPost httpPost = new HttpPost(URL_ADDRESS);
 		JSONObject jsonPostObject = new JSONObject();
 
 		try {
+			URL url = new URL(URL_ADDRESS);
 			jsonPostObject.accumulate("email", email);
 			JSONArray jsonPostArray = new JSONArray();
 			jsonPostArray.put(jsonPostObject);
-			StringEntity se = new StringEntity(jsonPostArray.toString());
-			httpPost.setEntity(se);
-			httpPost.setHeader("Content-type", "application/json");
-			HttpResponse response = httpClient.execute(httpPost);
-			StatusLine statusLine = response.getStatusLine();
-			int statusCode = statusLine.getStatusCode();
-			
-			if (statusCode == 200) {
-				HttpEntity entity = response.getEntity();
-				InputStream inputStream = entity.getContent();
-				BufferedReader reader = new BufferedReader(
-						new InputStreamReader(inputStream));
-				String line;
-				while ((line = reader.readLine()) != null) {
-					stringBuilder.append(line);
+			String jsonPostString = jsonPostArray.toString();
+			String responseString = null;
+
+			HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+			try {
+				urlConnection.setDoInput(true);
+				urlConnection.setDoOutput(true);
+				urlConnection.setRequestMethod("POST");
+				urlConnection.setRequestProperty("Content-Type", "application/json");
+				urlConnection.setRequestProperty("Accept", "application/json");
+				OutputStream os = new BufferedOutputStream((urlConnection.getOutputStream()));
+				writeStream(os, jsonPostString);
+
+				int statusCode = urlConnection.getResponseCode();
+				if (statusCode == 200) {
+					InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+					responseString = readStream(in);
+				} else {
+					System.out.println("Server error:" + statusCode + " " + urlConnection.getResponseMessage());
+					TurfWidget.setError("server");
 				}
-				inputStream.close();
-			} else {
-				System.out.println(statusLine.toString());
+			} finally {
+				urlConnection.disconnect();
 			}
 
-			JSONArray jsonArray = new JSONArray(stringBuilder.toString());
+			JSONArray jsonArray = new JSONArray(responseString);
 
 			// Get first and only user from array.
-            // FIXME: If the array is empty we could handle this better.
 			JSONObject jsonObject = jsonArray.getJSONObject(0);
 
 			CharStats charStats = new CharStats();
 			CharStats.setPoints(Integer.parseInt(jsonObject.getString("points")));
 			CharStats.setHour(Integer.parseInt(jsonObject.getString("pointsPerHour")));
-            // CharStats.setHour(12345); // Layout testing
+			// CharStats.setHour(12345); // Layout testing
 			CharStats.setPlace(Integer.parseInt(jsonObject.getString("place")));
 			// zones is an array, needs to be counted
 			JSONArray zoneArray = jsonObject.getJSONArray("zones");
 			CharStats.setZones(zoneArray.length());
-            // CharStats.setZones(12); // Layout testing
+			// CharStats.setZones(12); // Layout testing
 			CharStats.setAccount(true);
 
 			if (TurfWidget.DEBUG) {
@@ -113,5 +111,24 @@ public class ParseJSON {
 		}
 
 		return new CharStats();
+	}
+
+	private static void writeStream(OutputStream os, String output) throws IOException {
+		os.write(output.getBytes());
+		os.flush();
+	}
+
+	private static String readStream(InputStream is) throws IOException {
+		final int bufferSize = 1024;
+		final char[] buffer = new char[bufferSize];
+		final StringBuilder out = new StringBuilder();
+		Reader in = new InputStreamReader(is, "UTF-8");
+		for (; ; ) {
+			int rsz = in.read(buffer, 0, buffer.length);
+			if (rsz < 0)
+				break;
+			out.append(buffer, 0, rsz);
+		}
+		return out.toString();
 	}
 }
